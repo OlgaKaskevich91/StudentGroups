3d273c565825e9c873a7a3943981df0dd3f55cd5
Olga Kaskevich <kaskevich.olga@list.ru>
2024-08-20 22:08:45 +0300

    Разрешение конфликта и сохранение изменений

---
+++ src/main/java/nuclearSubmarine/AtomicSubmarine.java
@@ -0,0 +1,53 @@
+package nuclearSubmarine;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface SubmarineInfo {
+    String description();
+
+    int weight();
+}
+
+@SubmarineInfo(
+        description = "Torpedo-class nuclear submarine",
+        weight = 150000
+)
+public class AtomicSubmarine {
+    private String name;
+    private EngineAtomicSub engine;
+
+    public AtomicSubmarine(String name) {
+        this.name = name;
+        this.engine = new EngineAtomicSub();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEngine(EngineAtomicSub engine) {
+        this.engine = engine;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void startSailing() {
+        System.out.println(name + " start sailing...");
+        engine.start();
+    }
+
+    static class EngineAtomicSub {
+        public void start() {
+            System.out.println("Nuclear submarine engine started.");
+        }
+    }
+
+}
+
+
