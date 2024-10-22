dffcc941a414f71985d0e128c69db66132f66524
Olga Kaskevich <kaskevich.olga@list.ru>
2024-10-22 13:04:57 +0300

    fix:Added changes to exception handling,keywords have become a constant value,used Set<String> and Map<String, Integer> keywords object,renamed the variables.

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -5,47 +5,45 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String[] keywords = {"abstract", "assert", "boolean", "break", "byte", "case",
-                "catch", "char", "class", "const", "continue", "default", "do", "double",
-                "else", "enum", "extends", "final", "finally", "float", "for", "if",
-                "implements", "import", "instanceof", "int", "interface", "long", "native",
-                "new", "package", "private", "protected", "public", "return", "short",
-                "static", "strictfp", "super", "switch", "synchronized", "this", "throw",
-                "throws", "transient", "try", "void", "volatile", "while"};
-        int[] keywordCount = new int[keywords.length];
+    private static final Set<String> KEYWORDS = new HashSet<>(Arrays.asList("abstract", "assert", "boolean", "break", "byte", "case",
+            "catch", "char", "class", "const", "continue", "default", "do", "double",
+            "else", "enum", "extends", "final", "finally", "float", "for", "if",
+            "implements", "import", "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public", "return", "short",
+            "static", "strictfp", "super", "switch", "synchronized", "this", "throw",
+            "throws", "transient", "try", "void", "volatile", "while"));
+
+    public static void main(String[] args) {
+        Map<String, Integer> keywordCount = new HashMap<>();
 
         File inputFile = new File("src/main/java/streams/KeyContaining.java");
         File outputFile = new File("src/main/java/streams/key.txt");
-
 
         try (FileInputStream inputStream = new FileInputStream(inputFile);
              FileOutputStream outputStream = new FileOutputStream(outputFile)) {
 
             String content = new String(inputStream.readAllBytes());
-            String regex = content.replaceAll("\\s*\\p{Punct}+\\s*$", "");
-            String[] words = regex.split("\\s");
+            String cleanContent = content.replaceAll("\\s*\\p{Punct}+\\s*$", "");
+            String[] words = cleanContent.split("\\s");
 
             for (String word : words) {
-                for (int i = 0; i < keywords.length; i++) {
-                    if (keywords[i].equals(word)) {
-                        keywordCount[i]++;
-                        break;
-                    }
-
+                if (KEYWORDS.contains(word)) {
+                    keywordCount.merge(word, 1, Integer::sum);
                 }
             }
-            for (int i = 0; i < keywords.length; i++) {
-                if (keywordCount[i] > 0) {
-                    String line = keywords[i] + " : " + keywordCount[i] + "\n";
+            keywordCount.forEach((key, value) -> {
+                String line = key + " : " + value + "\n";
+                try {
                     outputStream.write(line.getBytes(StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    System.out.println("Error" + e.getMessage());
                 }
-            }
-
+            });
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
         }
-
-
     }
 }
