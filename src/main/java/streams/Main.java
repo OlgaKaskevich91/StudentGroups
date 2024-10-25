af26cc29fcdb99dcc1983c2dc034c87f7542e50c
Olga Kaskevich <kaskevich.olga@list.ru>
2024-10-25 15:48:17 +0300

    fix: Changes have been made to the code, search and keyword counting methods have been added. feat:Added tests.

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -1,9 +1,6 @@
 package streams;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -17,15 +14,27 @@ public class Main {
             "throws", "transient", "try", "void", "volatile", "while"));
 
     public static void main(String[] args) {
-        Map<String, Integer> keywordCount = new HashMap<>();
-
         File inputFile = new File("src/main/java/streams/KeyContaining.java");
         File outputFile = new File("src/main/java/streams/key.txt");
 
         try (FileInputStream inputStream = new FileInputStream(inputFile);
-             FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+             FileOutputStream outputStream = new FileOutputStream(outputFile);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream)) {
 
-            String content = new String(inputStream.readAllBytes());
+            Map<String, Integer> keywordCount = countKeywords(bufferedInputStream);
+            writeKeywordsCount(bufferedOutputStream, keywordCount);
+
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+
+    static Map<String, Integer> countKeywords(BufferedInputStream bufferedInputStream) {
+        Map<String, Integer> keywordCount = new HashMap<>();
+        try {
+            byte[] buffer = bufferedInputStream.readAllBytes();
+            String content = new String(buffer, StandardCharsets.UTF_8);
             String cleanContent = content.replaceAll("\\s*\\p{Punct}+\\s*$", "");
             String[] words = cleanContent.split("\\s");
 
@@ -34,16 +43,20 @@ public class Main {
                     keywordCount.merge(word, 1, Integer::sum);
                 }
             }
-            keywordCount.forEach((key, value) -> {
-                String line = key + " : " + value + "\n";
-                try {
-                    outputStream.write(line.getBytes(StandardCharsets.UTF_8));
-                } catch (IOException e) {
-                    System.out.println("Error" + e.getMessage());
-                }
-            });
         } catch (IOException e) {
             System.out.println("Error" + e.getMessage());
         }
+        return keywordCount;
+    }
+
+    private static void writeKeywordsCount(BufferedOutputStream bufferedOutputStream, Map<String, Integer> keywordCount) {
+        keywordCount.forEach((key, value) -> {
+            String line = key + " : " + value + "\n";
+            try {
+                bufferedOutputStream.write(line.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                System.out.println("Error" + e.getMessage());
+            }
+        });
     }
 }
