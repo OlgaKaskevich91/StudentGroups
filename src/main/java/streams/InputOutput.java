1a30e491af2bada1f036f290cda09fd7b834a2b1
Olga Kaskevich <kaskevich.olga@list.ru>
2024-11-05 18:20:46 +0300

    feat: Working with byte and character I/O streams

---
+++ src/main/java/streams/InputOutput.java
@@ -0,0 +1,22 @@
+package streams;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        File inputFile = new File("src/main/java/streams/input.txt");
+        File outputFile = new File("src/main/java/streams/output.txt");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_16))) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Processing file error: " + e.getMessage());
+        }
+    }
+}
