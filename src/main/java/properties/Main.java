cae1ad9b40847fae98806cd35d0df89d4cb8358d
Olga Kaskevich <kaskevich.olga@list.ru>
2024-11-23 21:08:26 +0300

    feat: Exception handling with properties files.

---
+++ src/main/java/properties/Main.java
@@ -0,0 +1,13 @@
+package properties;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            PropertiesLoader loader = new PropertiesLoader("src/main/resources/file.properties");
+            String value = loader.getValue("dbU");
+            System.out.println("Value from the file: " + value);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/properties/PropertiesLoader.java
@@ -0,0 +1,30 @@
+package properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesLoader {
+    private final Properties properties = new Properties();
+
+    public PropertiesLoader(String filePath) {
+        loadProperties(filePath);
+    }
+
+    private void loadProperties(String filePath) {
+        try (InputStream input = new FileInputStream(filePath)) {
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error: failed to upload file " + e.getMessage());
+        }
+    }
+
+    public String getValue(String key) {
+        String value = properties.getProperty(key);
+        if (value == null) {
+            throw new RuntimeException("Key \"" + key + "\" not found in file");
+        }
+        return value;
+    }
+}
